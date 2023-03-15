d556d187632b8be56592ad4e78fdf5a3e419f5b7
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-03-15 10:06:09 +0200

    feat: basic ui for join or create meeting

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ import './App.css'
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
+    <div className='h-screen flex justify-center items-center columns-1'>
+      <div>
+        <input type="text" id="meeting-code" className='block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' placeholder='Meeting ID'/>
+      </div>
+      <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+          <button type="button" class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">New Meeting</button>
+          <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Join</button>
+        </div>
+    </div>
   )
 }
 
